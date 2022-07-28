532b1b514e5086c9b97b0e1645e36b41f04cdfc4
Jeff <77259593+Cloudlearning2121@users.noreply.github.com>
2022-07-28 16:41:09 -0500

    Add files via upload

---
+++ Providers.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-02d1e544b84bf7502" # Ubuntu 20.04 LTS // us-east-1
+  instance_type = "t2.micro"
+}
